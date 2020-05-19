375969fa45022e2670ec2e97f426e7997d5ea23f
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-19 09:12:41 +0200

    Remove the word 'locatome'

--- R/default_params_doc.R
+++ R/default_params_doc.R
@@ -7,8 +7,6 @@
 #'   The superfolder's name is \code{/home/[user_name]/.local/share}
 #'   by default, as can be obtained by
 #'   \link{get_default_pureseqtm_folder}
-#' @param locatome text similar to a FASTA file, with protein names
-#'   and the locations of its amino acids
 #' @param protein_sequence a protein sequence
 #' @param protein_sequences one or more protein sequences
 #' @param pureseqtm_filename filename to write the PureseqTM results to
@@ -34,7 +32,6 @@ default_params_doc <- function(
   fasta_filename,
   fasta_file_text,
   folder_name,
-  locatome,
   protein_sequence,
   protein_sequences,
   pureseqtm_filename,
