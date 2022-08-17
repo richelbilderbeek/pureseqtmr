57c0973cbfc650bb34c59d25db70de4ede917f13
richelbilderbeek <richel@richelbilderbeek.nl>
2022-08-17 13:22:15 +0200

    Export 'convert_tmhmm_to_pureseqtm_topology'

--- R/convert_tmhmm_to_pureseqtm_topology.R
+++ R/convert_tmhmm_to_pureseqtm_topology.R
@@ -1,3 +1,15 @@
+#' Convert a TMHMM topology to a PureseqTM topology
+#' @inheritParams default_params_doc
+#' @return a \link[tibble]{tibble} with column names
+#' `name` and `topology`, as can be checked by \link{check_topology}
+#' @examples
+#' tmhmm_topo_filename <- system.file(
+#'   "extdata", "UP000005640_9606_no_u.tmhmm", package = "pureseqtmr"
+#' )
+#' tmhmm_topology <- load_topology_file_as_tibble(tmhmm_topo_filename)
+#' convert_tmhmm_to_pureseqtm_topology(tmhmm_topology)
+#' @author Richèl J.C. Bilderbeek
+#' @export
 convert_tmhmm_to_pureseqtm_topology <- function(tmhmm_topology) {
   pureseqtmr::check_topology(tmhmm_topology)
   tmhmm_topology$topology <- stringr::str_replace_all(
