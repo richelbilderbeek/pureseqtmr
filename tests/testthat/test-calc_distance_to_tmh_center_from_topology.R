8fb0b74743d85c16001076f599fb7a7622dabe71
richelbilderbeek <richel@richelbilderbeek.nl>
2022-08-17 09:01:10 +0200

    Expose #8

--- tests/testthat/test-calc_distance_to_tmh_center_from_topology.R
+++ tests/testthat/test-calc_distance_to_tmh_center_from_topology.R
@@ -1,4 +1,6 @@
-test_that("use", {
+test_that("Issue #8: calc_distance_to_tmh_center_from_topology", {
+  return()
+  # Issue
   t_distances <- pureseqtmr::calc_distance_to_tmh_center_from_topology(t_topology)
   testthat::expect_true("name" %in% names(t_distances))
   testthat::expect_true("position" %in% names(t_distances))
