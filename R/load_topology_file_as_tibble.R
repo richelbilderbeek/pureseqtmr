ff30492abe6c94edb320822babc3ac305f49cf5c
richelbilderbeek <richel@richelbilderbeek.nl>
2022-08-17 12:49:29 +0200

    Fix doc

--- R/load_topology_file_as_tibble.R
+++ R/load_topology_file_as_tibble.R
@@ -5,16 +5,16 @@
 #' with a \code{name} and \code{topology} column,
 #' as can be checked by \link{check_topology}
 #' @examples
-#' topo_filename <- system.file(
+#' topology_filename <- system.file(
 #'  "extdata", "100507436.topo", package = "pureseqtmr"
 #' )
-#' load_topology_file_as_tibble(topo_filename)
+#' load_topology_file_as_tibble(topology_filename)
 #' @export
 load_topology_file_as_tibble <- function(
-  topo_filename
+    topology_filename
 ) {
   topology <- pureseqtmr::load_fasta_file_as_tibble_cpp(
-    normalizePath(topo_filename)
+    normalizePath(topology_filename)
   )
   testthat::expect_equal(names(topology), c("name", "sequence"))
   names(topology) <- c("name", "topology")
